dceaa01efa8a5dec340cda0d275f82a7b86b8980
severak <severak@users.noreply.github.com>
2014-09-14 18:25:38 +0200

    Pridano Mustache. Zatim nepouzito.

---
+++ dynamo-lib/Packfire/Template/Mustache/Mustache.php
@@ -0,0 +1,413 @@
+<?php
+
+/**
+ * Packfire Framework for PHP
+ * By Sam-Mauris Yong
+ *
+ * Released open source under New BSD 3-Clause License.
+ * Copyright (c) Sam-Mauris Yong <sam@mauris.sg>
+ * All rights reserved.
+ */
+
+namespace Packfire\Template\Mustache;
+
+/**
+ * A PHP implementation of Mustache, a simple logic-less templating system
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) Sam-Mauris Yong
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package Packfire\Template\Mustache
+ * @since 1.0-sofia
+ */
+class Mustache
+{
+    /**
+     * The template to be parsed
+     * @var string
+     * @since 1.0-sofia
+     */
+    protected $template;
+
+    /**
+     * The parameters to work with
+     * @var mixed
+     * @since 1.0-sofia
+     */
+    protected $parameters;
+
+    /**
+     * The partials loader
+     * @var Packfire\Template\Mustache\LoaderInterface
+     * @since 1.1.0
+     */
+    protected $loader;
+
+    /**
+     * The escaper callback
+     * @var Closure|callback
+     * @since 1.0-sofia
+     */
+    protected $escaper = array(__CLASS__, 'escape');
+
+    /**
+     * The opening delimiter
+     * @var string
+     * @since 1.0.1
+     */
+    protected $openDelimiter = '{{';
+
+    /**
+     * The closing delimiter
+     * @var string
+     * @since 1.0.1
+     */
+    protected $closeDelimiter = '}}';
+
+    /**
+     * The current processing line number
+     * @var string
+     * @since 1.2.0
+     */
+    protected $line = 1;
+
+    /**
+     * The current number of tokens on the line
+     * @var string
+     * @since 1.2.0
+     */
+    protected $lineToken = 0;
+
+    /**
+     * Create a new Mustache object
+     * @param string $template (optional) Set the template to render
+     * @param array $options (optional) Set a variety of options
+     * @since 1.0-sofia
+     */
+    public function __construct($template = null, array $options = array())
+    {
+        $this->template = $template;
+
+        if (array_key_exists('loader', $options) && $options['loader'] instanceof LoaderInterface) {
+            $this->loader = $options['loader'];
+        }
+
+        if (array_key_exists('escaper', $options) && is_callable($options['escaper'])) {
+            $this->escaper = $options['escaper'];
+        }
+
+        if (array_key_exists('open', $options)) {
+            $this->openDelimiter = $options['open'];
+        }
+
+        if (array_key_exists('close', $options)) {
+            $this->closeDelimiter = $options['close'];
+        }
+    }
+
+    /**
+     * Default escaper for escaping the text using the default
+     * htmlspecialchars() function.
+     * @param string $text The text to be escaped
+     * @return string The escaped text.
+     * @since 1.1.0
+     */
+    protected static function escape($text)
+    {
+        return htmlspecialchars($text, ENT_COMPAT, 'UTF-8');
+    }
+
+    /**
+     * Perform parsing of a scope
+     * @param array $scope The parameter scope to work with
+     * @param array $tokens The array of tokens to parse
+     * @return string Returns the parsed text
+     * @since 1.2.0
+     */
+    private function parse(array $scope, array $tokens)
+    {
+        $buffer = '';
+        $line = array();
+        foreach ($tokens as $token) {
+            if ($token[Tokenizer::TOKEN_LINE] === $this->line) {
+                ++$this->lineToken;
+                $line[] = $token;
+            } else {
+                $buffer .= $this->processLine($scope, $line);
+                $line = array($token);
+                $this->lineToken = 1;
+                $this->line = $token[Tokenizer::TOKEN_LINE];
+            }
+        }
+        if ($line) {
+            $buffer .= $this->processLine($scope, $line);
+        }
+        return $buffer;
+    }
+
+    private function processLine(array $scope, array $tokens)
+    {
+        $buffer = '';
+        if (count($tokens) == 2) {
+            if (self::isTokenStandaloneClear($tokens[0])
+                    && $tokens[1][Tokenizer::TOKEN_TYPE] == Tokenizer::TYPE_LINE) {
+                array_pop($tokens);
+            }
+        } elseif (count($tokens) == 3) {
+            if (self::isTokenWhitespace($tokens[0])
+                    && self::isTokenStandaloneClear($tokens[1])
+                    && $tokens[2][Tokenizer::TOKEN_TYPE] == Tokenizer::TYPE_LINE) {
+                array_shift($tokens);
+                array_pop($tokens);
+            }
+        }
+        foreach ($tokens as $token) {
+            switch ($token[Tokenizer::TOKEN_TYPE]) {
+                case Tokenizer::TYPE_OPEN:
+                    $name = $token[Tokenizer::TOKEN_NAME];
+                    $property = $this->scope(array_merge($scope, array($name)));
+                    if ($property) {
+                        if ($this->isArrayOfObjects($property)) {
+                            foreach ($property as $idx => $item) {
+                                $path = array_merge($scope, array($name, $idx));
+                                $buffer .= $this->parse($path, $token[Tokenizer::TOKEN_NODES]);
+                            }
+                        } else {
+                            $path = $scope;
+                            if (!is_scalar($property)) {
+                                $path = array_merge($scope, array($name));
+                            }
+                            $buffer .= $this->parse($path, $token[Tokenizer::TOKEN_NODES]);
+                        }
+                    }
+                    break;
+                case Tokenizer::TYPE_CLOSE:
+                    throw new MustacheException('Unexpected close tag ' . $token[Tokenizer::TOKEN_OPEN_DELIMITER] . '/' . $token[Tokenizer::TOKEN_NAME] . $token[Tokenizer::TOKEN_CLOSE_DELIMITER] . ' found at line ' . $this->line . '.');
+                    break;
+                case Tokenizer::TYPE_INVERT:
+                    $name = $token[Tokenizer::TOKEN_NAME];
+                    $property = $this->scope(array_merge($scope, array($name)));
+                    if (!$property) {
+                        $buffer .= $this->parse($scope, $token[Tokenizer::TOKEN_NODES]);
+                    }
+                    break;
+                case Tokenizer::TYPE_NORMAL:
+                    $name = $token[Tokenizer::TOKEN_NAME];
+                    if ($name == '.') {
+                        $property = $this->scope($scope);
+                    } else {
+                        $property = $this->scope(array_merge($scope, array($name)));
+                    }
+                    if ($property) {
+                        if (is_array($property)) {
+                            $property = implode('', $property);
+                        }
+                        $buffer .= call_user_func($this->escaper, $property);
+                    }
+                    break;
+                case Tokenizer::TYPE_UNESCAPETRIPLE:
+                case Tokenizer::TYPE_UNESCAPE:
+                    $name = $token[Tokenizer::TOKEN_NAME];
+                    $property = $this->scope(array_merge($scope, array($name)));
+                    if ($property) {
+                        if (is_array($property)) {
+                            $property = implode('', $property);
+                        }
+                        $buffer .= $property;
+                    }
+                    break;
+                case Tokenizer::TYPE_TEXT:
+                    $buffer .= $token[Tokenizer::TOKEN_VALUE];
+                    break;
+                case Tokenizer::TYPE_LINE:
+                    $buffer .= $token[Tokenizer::TOKEN_VALUE];
+                    break;
+                case Tokenizer::TYPE_PARTIAL1:
+                case Tokenizer::TYPE_PARTIAL2:
+                    $name = $token[Tokenizer::TOKEN_NAME];
+                    $buffer .= $this->partial($name, $this->scope($scope));
+                    break;
+            }
+        }
+        return $buffer;
+    }
+
+    public static function isTokenStandaloneClear(array $token)
+    {
+        $types = array(
+            Tokenizer::TYPE_COMMENT,
+            Tokenizer::TYPE_CHANGETAG,
+            Tokenizer::TYPE_OPEN,
+            Tokenizer::TYPE_CLOSE,
+            Tokenizer::TYPE_COMMENT,
+            Tokenizer::TYPE_PARTIAL1,
+            Tokenizer::TYPE_PARTIAL2,
+            Tokenizer::TYPE_INVERT
+        );
+        return in_array($token[Tokenizer::TOKEN_TYPE], $types);
+    }
+
+    public static function isTokenWhitespace(array $token)
+    {
+        if ($token[Tokenizer::TOKEN_TYPE] == Tokenizer::TYPE_TEXT) {
+            return preg_match('/^\s*$/', $token[Tokenizer::TOKEN_VALUE]);
+        }
+
+        return false;
+    }
+
+    protected function scope($path)
+    {
+        $path = self::processDotNotation($path);
+        $originalPath = $path;
+        $scope =  $this->parameters;
+        while (count($path) > 0) {
+            $scope = $this->parameters;
+            foreach ($path as $item) {
+                $scope = (array)$scope;
+                if (isset($scope[$item])) {
+                    $scope = $scope[$item];
+                } else {
+                    $scope = null;
+                    break;
+                }
+            }
+            if ($scope !== null) {
+                break;
+            }
+            array_shift($path);
+        }
+        if (is_callable($scope)) {
+            $scope = call_user_func($scope);
+        }
+        return $scope;
+    }
+
+    protected static function processDotNotation($path)
+    {
+        $result = array();
+        foreach ($path as $item) {
+            $items = explode('.', $item);
+            $result = array_merge($result, $items);
+        }
+        return $result;
+    }
+
+    /**
+     * Set the partial loader for the Mustache engine
+     * @param Packfire\Template\Mustache\LoaderInterface $loader The loader for partials
+     * @return Mustache Returns self for chaining
+     * @since 1.1.0
+     */
+    public function loader(LoaderInterface $loader)
+    {
+        $this->loader = $loader;
+        return $this;
+    }
+
+    /**
+     * Set the escaping callback
+     * @param callback $escaper The callback function / method for providing escaping
+     * @return Mustache Returns self for chaining
+     * @since 1.1.0
+     */
+    public function escaper($escaper)
+    {
+        $this->escaper = $escaper;
+        return $this;
+    }
+
+    /**
+     * Get the partial by name and add to the buffer
+     * @param string $name Name of the partial
+     * @return string Returns the rendered buffer
+     * @since 1.0-sofia
+     */
+    protected function partial($name, $scope)
+    {
+        $buffer = '';
+        if ($this->loader) {
+            $template = $this->loader->load($name);
+            if ($template) {
+                $partial = new Mustache($template);
+                $partial->parameters($this->parameters)
+                        ->loader($this->loader)
+                        ->escaper($this->escaper);
+                $buffer .= $partial->render($scope);
+            }
+        }
+        return $buffer;
+    }
+
+    /**
+     * Check if the scope is an array of objects
+     * @param mixed $value The value to be checked
+     * @return boolean Returns true if the value is an array of objects,
+     *                  false otherwise.
+     * @since 1.0-sofia
+     */
+    private function isArrayOfObjects($value)
+    {
+        if (is_object($value)) {
+            return $value instanceof \Traversable;
+        } elseif (is_array($value)) {
+            return array_keys($value) === range(0, count($value) - 1);
+        }
+        return false;
+    }
+
+    /**
+     * Set the template to be rendered by Mustache
+     * @param string $template The template to render
+     * @return Mustache Returns self for chaining
+     * @since 1.0-sofia
+     */
+    public function template($template)
+    {
+        $this->template = $template;
+        return $this;
+    }
+
+    /**
+     * Set the parameters to be rendered into the template
+     * @param array $parameters The parameters
+     * @return Mustache Returns self for chaining
+     * @since 1.0-sofia
+     */
+    public function parameters($parameters)
+    {
+        $this->parameters = $parameters;
+        return $this;
+    }
+
+    /**
+     * Performs preparation of parameters
+     * @since 1.1-sofia
+     */
+    protected function loadParameters()
+    {
+        if (get_class($this) != __CLASS__) {
+            $this->parameters = $this;
+        }
+        if (count($this->parameters) == 0) {
+            $this->parameters = null;
+        }
+    }
+
+    /**
+     * Render the Mustache template
+     * @param array $scope (optional) The scope to start working from on the parameters.
+     * @return string Returns the parsed template
+     * @since 1.0-sofia
+     */
+    public function render($scope = array())
+    {
+        $this->loadParameters();
+        $tokenizer = new Tokenizer();
+        $tokenizer->changeDelimiters($this->openDelimiter, $this->closeDelimiter);
+        $tokens = $tokenizer->parse($this->template);
+        $this->line = 1;
+        $buffer = $this->parse($scope, $tokens);
+        return $buffer;
+    }
+}
---
+++ dynamo-lib/Packfire/Template/Mustache/Tokenizer.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace Packfire\Template\Mustache;
+
+class Tokenizer
+{
+    /**
+     * The tag regular expression
+     * @since 1.2.0
+     */
+    const TAG_REGEX = '`%s([%s]{0,1})(%s)%s`is';
+
+    const TYPE_NORMAL = '!ev';
+    const TYPE_OPEN = '#';
+    const TYPE_CLOSE = '/';
+    const TYPE_UNESCAPE = '&';
+    const TYPE_UNESCAPETRIPLE = '{';
+    const TYPE_INVERT = '^';
+    const TYPE_COMMENT = '!';
+    const TYPE_PARTIAL1 = '>';
+    const TYPE_PARTIAL2 = '<';
+    const TYPE_CHANGETAG = '=';
+    const TYPE_TEXT = '!t';
+    const TYPE_LINE = '!l';
+
+    const TOKEN_TYPE = 'type';
+    const TOKEN_NAME = 'name';
+    const TOKEN_OPEN_DELIMITER = 'open';
+    const TOKEN_CLOSE_DELIMITER = 'close';
+    const TOKEN_POSITION = 'position';
+    const TOKEN_LINE = 'line';
+    const TOKEN_COLUMN = 'column';
+    const TOKEN_VALUE = 'value';
+    const TOKEN_NODES = 'nodes';
+
+    /**
+     * The opening delimiter
+     * @var string
+     * @since 1.2.0
+     */
+    protected $openDelimiter = '{{';
+
+    /**
+     * The closing delimiter
+     * @var string
+     * @since 1.2.0
+     */
+    protected $closeDelimiter = '}}';
+
+    /**
+     * The current line number
+     * @var integer
+     * @since 1.2.0
+     */
+    protected $line = 1;
+
+    public function parse($text)
+    {
+        $tokens = array();
+        $textLength = strlen($text);
+        $position = 0;
+        $this->line = 1;
+        while ($position < $textLength) {
+            $match = array();
+            $newlinePosition = strpos($text, "\n", $position);
+            if ($newlinePosition === false) {
+                $newlinePosition = $textLength;
+            }
+            do {
+                $hasTagMatch = preg_match(
+                    $this->buildMatchingTag(),
+                    substr($text, 0, $newlinePosition),
+                    $match,
+                    PREG_OFFSET_CAPTURE,
+                    $position
+                );
+                if ($hasTagMatch) {
+                    // there's a tag in between
+                    $tagLength = strlen($match[0][0]);
+                    $tagStart = $match[0][1];
+                    $tagEnd = $tagStart + $tagLength;
+                    if ($match[1][0] == self::TYPE_UNESCAPETRIPLE) {
+                        $tagLength += 1;
+                        $tagEnd += 1;
+                    }
+
+                    $subText = substr($text, $position, $tagStart - $position);
+                    if (strlen($subText)) {
+                        $tokens[] = array(
+                            self::TOKEN_TYPE => self::TYPE_TEXT,
+                            self::TOKEN_LINE => $this->line,
+                            self::TOKEN_VALUE => $subText
+                        );
+                    }
+
+                    $tokens[] = $this->buildTagToken($match, $position);
+                    $position = $tagEnd;
+                }
+            } while ($hasTagMatch);
+
+            $subText = substr($text, $position, $newlinePosition - $position);
+            if (strlen($subText)) {
+                $tokens[] = array(
+                    self::TOKEN_TYPE => self::TYPE_TEXT,
+                    self::TOKEN_LINE => $this->line,
+                    self::TOKEN_VALUE => $subText
+                );
+            }
+            $position = $newlinePosition;
+            if (substr($text, $position, 1) === "\n") {
+                $tokens[] = array(
+                    self::TOKEN_TYPE => self::TYPE_LINE,
+                    self::TOKEN_LINE => $this->line,
+                    self::TOKEN_VALUE => "\n"
+                );
+                ++$this->line;
+                ++$position;
+            }
+        }
+        $tokens = $this->processTokens($tokens);
+        $this->reset();
+        return $tokens;
+    }
+
+    public function changeDelimiters($open, $close)
+    {
+        $this->openDelimiter = $open;
+        $this->closeDelimiter = $close;
+    }
+
+    public function reset()
+    {
+        $this->openDelimiter = '{{';
+        $this->closeDelimiter = '}}';
+        $this->line = 1;
+    }
+
+    protected function processTokens($tokens, &$index = 0, $closingTag = null)
+    {
+        $result = array();
+        $count = count($tokens);
+        for (; $index < $count; ++$index) {
+            $token = $tokens[$index];
+            if ($token[self::TOKEN_TYPE] == self::TYPE_OPEN
+                || $token[self::TOKEN_TYPE] == self::TYPE_INVERT) {
+                self::clearStandalone($result, $index, $tokens);
+                ++$index;
+                $nodes = $this->processTokens($tokens, $index, $token[self::TOKEN_NAME]);
+                $token[self::TOKEN_NODES] = $nodes;
+                $result[] = $token;
+            } elseif ($closingTag && $token[self::TOKEN_TYPE] == self::TYPE_CLOSE && $token[self::TOKEN_NAME] == $closingTag) {
+                self::clearStandalone($result, $index, $tokens);
+                break;
+            } else {
+                $result[] = $token;
+            }
+        }
+        return $result;
+    }
+
+    protected static function clearStandalone(&$result, &$index, &$tokens)
+    {
+        if ($index >= 1 && $index < count($tokens) - 1
+                && $tokens[$index - 1][self::TOKEN_TYPE] == self::TYPE_LINE
+                && $tokens[$index + 1][self::TOKEN_TYPE] == self::TYPE_LINE) {
+            ++$index;
+        } elseif ($index >= 2 && $index < count($tokens) - 1
+                && $tokens[$index - 2][self::TOKEN_TYPE] == self::TYPE_LINE
+                && Mustache::isTokenWhitespace($tokens[$index - 1])
+                && $tokens[$index + 1][self::TOKEN_TYPE] == self::TYPE_LINE) {
+            array_pop($result);
+            ++$index;
+        }
+    }
+
+    protected function buildTagToken($match, $position)
+    {
+        $name = $match[2][0];
+        $openDelimiter = $this->openDelimiter;
+        $closeDelimiter = $this->closeDelimiter;
+        if ($match[1][0] == self::TYPE_CHANGETAG) {
+            if (substr($name, -1) == '=') {
+                $name = substr($name, 0, strlen($name) - 1);
+            }
+            list($this->openDelimiter, $this->closeDelimiter) = explode(' ', $name);
+        }
+        $type = $match[1][0];
+        if (!$type) {
+            $type = self::TYPE_NORMAL;
+        }
+        return array(
+            self::TOKEN_TYPE => $type,
+            self::TOKEN_NAME => trim($name),
+            self::TOKEN_LINE => $this->line,
+            self::TOKEN_OPEN_DELIMITER => $openDelimiter,
+            self::TOKEN_CLOSE_DELIMITER => $closeDelimiter
+        );
+    }
+
+    /**
+     * Build the tag matching regular expression
+     * @param string $name (optional) The tag name to match
+     * @return string Returns the final regular expression
+     * @since 1.2.0
+     */
+    private function buildMatchingTag($name = '.+?', $type = '^/&#={!><')
+    {
+        return sprintf(self::TAG_REGEX, preg_quote($this->openDelimiter), preg_quote($type), $name, preg_quote($this->closeDelimiter));
+    }
+}
